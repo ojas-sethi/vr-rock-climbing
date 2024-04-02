35280a6333816f1ddf93ccff867d44e60544ed03
Adithya Varma <adithyavarmakonduru@gmail.com>
2024-04-02 13:37:04 -0500

    added gun. currently mulitplayer not functional

---
+++ unity-vr-rock-climbing/Assets/HandleGun/ConvertToHandle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToHandle : MonoBehaviour
+{
+	public GameObject handle;
+    void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "Climbable")
+		{
+			Debug.Log("Bullet Hit a wall");
+			ContactPoint contact = col.contacts[0];
+			//Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+			//Transform position = contact.point;
+			Vector3 colPos = contact.point;
+			GameObject spawnedHandle = Instantiate(handle);
+			//spawnedHandle.transform.position =  col.transform.position;
+			spawnedHandle.transform.position =  colPos;
+			//Instantiate(handle, position, rotation);
+			Destroy(gameObject);
+		}
+	}
+}
